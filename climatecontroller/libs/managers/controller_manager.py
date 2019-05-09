9a4355bd4e83f05e18df9e4756c02bebf268f2b2
nkitsan <nastik.kitsan@gmail.com>
2019-05-09 21:41:22 +0300

    Update work with deltas

--- climatecontroller/libs/managers/controller_manager.py
+++ climatecontroller/libs/managers/controller_manager.py
@@ -9,10 +9,6 @@ from climatecontroller.libs.parsers import request_parser
 from climatecontroller.libs.helpers import params_helper
 
 
-DELTA = 1.0
-MAX_CO2_CONCENTRATION = 10000
-MIN_CO2_CONCENTRATION = 2000
-
 def control_climate(data):
     if params_helper.temperature in data:
         manage_climate_state(data[params_helper.temperature], 
@@ -30,12 +26,12 @@ def manage_climate_state(current_value, controller):
         state = request_manager.get_controller_state(controller.controller_ip, 
                                                      controller.controller_gpio)
 
-        if controller.preffered_value - current_value > DELTA and state == 0:
+        if controller.preffered_value - current_value > controller.delta and state == 0:
             request_manager.change_controller_state(controller.controller_ip, 
                                                     controller.controller_gpio,
                                                     1)
 
-        else if current_value - controller.preffered_value > DELTA and state == 1:
+        elif current_value - controller.preffered_value > controller.delta and state == 1:
             request_manager.change_controller_state(controller.controller_ip, 
                                                     controller.controller_gpio,
                                                     0)
@@ -47,12 +43,12 @@ def manage_carbondioxide(current_value):
         state = request_manager.get_controller_state(controller.controller_ip, 
                                                      controller.controller_gpio)
 
-        if current_value > MAX_CO2_CONCENTRATION and state == 0:
+        if current_value > controller.max_value and state == 0:
             request_manager.change_controller_state(controller.controller_ip, 
                                                     controller.controller_gpio,
                                                     1)
 
-        else if current_value < MIN_CO2_CONCENTRATION and state == 1:
+        elif current_value < controller.min_value and state == 1:
             request_manager.change_controller_state(controller.controller_ip, 
                                                     controller.controller_gpio,
                                                     0)
--- climatecontroller/models.py
+++ climatecontroller/models.py
@@ -7,17 +7,21 @@ class HumidityController(models.Model):
 	controller_gpio = models.PositiveSmallIntegerField(blank=False, null=False)
 	preffered_value = models.FloatField(validators=[MinValueValidator(30.0), MaxValueValidator(60.0)], default=30.0)
 	control_mode = models.BooleanField(default=True)
+	delta = models.FloatField(validators=[MinValueValidator(0.0), MaxValueValidator(5.0)], default=1.0)
 	
 class TemperatureController(models.Model):
 	controller_ip = models.GenericIPAddressField()
 	controller_gpio = models.PositiveSmallIntegerField(blank=False, null=False)
 	preffered_value = models.FloatField(validators=[MinValueValidator(16.0), MaxValueValidator(30.0)], default=22.0)
 	control_mode = models.BooleanField(default=True)
+	delta = models.FloatField(validators=[MinValueValidator(0.0), MaxValueValidator(5.0)], default=1.0)
 
 class CarbonDioxideController(models.Model):
 	controller_ip = models.GenericIPAddressField()
 	controller_gpio = models.PositiveSmallIntegerField(blank=False, null=False)
 	control_mode = models.BooleanField(default=True)
+	max_value = models.FloatField(validators=[MinValueValidator(2000.0), MaxValueValidator(10000.0)], default=10000.0)
+	min_value = models.FloatField(validators=[MinValueValidator(2000.0), MaxValueValidator(10000.0)], default=2000.0)
 						
 class Humidity(models.Model):
 	time = models.DateTimeField()
